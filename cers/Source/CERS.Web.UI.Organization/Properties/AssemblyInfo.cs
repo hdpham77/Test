7e3f8ac2fa23941e5da20760556341bbb4e293d9
hdpham77 <35605599+hdpham77@users.noreply.github.com>
2018-01-30 08:08:45 -0800

    Add files via upload

---
+++ cers/Source/CERS.Web.UI.Organization/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CERS.Web.UI.Organization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("CERS.Web.UI.Organization")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("baef15c9-d4b3-41bc-9815-322cfe19debf")]
