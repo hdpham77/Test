777f66b67fd1794f4535eab5fefb5e44dd169c7f
hdpham77 <35605599+hdpham77@users.noreply.github.com>
2018-01-30 06:36:28 -0800

    Add files via upload

---
+++ cers/VersionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany( "State of California, California Environmental Protection Agency" )]
+[assembly: AssemblyCopyright( "Copyright © California Environmental Protection Agency 2015" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyConfiguration( "" )]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version Minor Version [CERS PADS THIS portion to two characters, with a leading zero if
+// need be] Build Number [CERS IGNORES THIS IN THE UI] Revision [CERS PADS THIS portion to 4
+// characteres, with leading zeros if need be]
+//
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "2.23.0.0017" )]
+[assembly: AssemblyFileVersion( "2.23.0.0017" )]
