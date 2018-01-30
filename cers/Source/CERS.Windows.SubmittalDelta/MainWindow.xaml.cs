7eb336349b85973e7ae027fd8fe44c980eabb157
hdpham77 <35605599+hdpham77@users.noreply.github.com>
2018-01-30 15:14:13 -0800

    Add files via upload

---
+++ cers/Source/CERS.Windows.SubmittalDelta/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using CERS;
+using CERS.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UPF;
+using UPF.Core;
+using UPF.Core.Cryptography;
+using UPF.Core.Model;
+
+namespace CERS.Windows.SubmittalDelta
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ICoreRepositoryManager _CoreDataRepository;
+		private ICERSRepositoryManager _DataRepository;
+		private ICERSRepositoryManager _Repository;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			CurrentAccountID = UPF.Core.Constants.DefaultAccountID;
+		}
+
+		public ICoreRepositoryManager CoreDataRepository
+		{
+			get
+			{
+				if ( _CoreDataRepository == null )
+				{
+					_CoreDataRepository = CoreServiceLocator.GetRepositoryManager( CurrentAccountID );
+				}
+				return _CoreDataRepository;
+			}
+		}
+
+		public int CurrentAccountID
+		{
+			get;
+			set;
+		}
+
+		public ICERSRepositoryManager DataRepository
+		{
+			get
+			{
+				if ( _DataRepository == null )
+				{
+					_DataRepository = ServiceLocator.GetRepositoryManager( CurrentAccountID );
+				}
+				return _DataRepository;
+			}
+		}
+
+		public ICERSRepositoryManager Repository
+		{
+			get
+			{
+				if ( _Repository == null )
+				{
+					_Repository = ServiceLocator.GetRepositoryManager();
+				}
+				return _Repository;
+			}
+		}
+
+		private IAccount MyAccount { get; set; }
+
+		public static AuthenticationResult Authenticate( string userName, string password, out Account account, bool passwordIsHashed = false )
+		{
+			if ( string.IsNullOrWhiteSpace( userName ) )
+			{
+				throw new ArgumentNullException( "userName" );
+			}
+
+			if ( string.IsNullOrWhiteSpace( password ) )
+			{
+				throw new ArgumentNullException( "password" );
+			}
+
+			int authenticationAttemptID = 0;
+			AuthenticationStatus result = AuthenticationStatus.Failure_Unknown;
+
+			using ( ICoreRepositoryManager repository = CoreServiceLocator.GetRepositoryManager() )
+			{
+				CoreEntities ctx = repository.DataModel;
+				account = null;
+
+				//lets patch up the username and remove any leading or trailing spaces.
+				userName = userName.Trim();
+
+				//lets look and see if the userName is an email address.
+				account = ctx.Accounts.SingleOrDefault( p => p.UserName == userName && !p.Voided );
+
+				if ( account != null )
+				{
+					//check account approved/disabled
+					result = account.Approved ? AuthenticationStatus.Success : AuthenticationStatus.Failure_AccountNotApproved;
+					result = account.Disabled ? AuthenticationStatus.Failure_AccountDisabled : AuthenticationStatus.Success;
+					result = account.Password == null ? AuthenticationStatus.Failure_NoPassword : AuthenticationStatus.Success;
+
+					//if so far we are approved and not disabled, and password is not null, then lets check the password against the input.
+					if ( result == AuthenticationStatus.Success && !VerifyPasswordMatch( password, account.Password, passwordIsHashed ) )
+					{
+						result = AuthenticationStatus.Failure_IncorrectPassword;
+					}
+				}
+				else
+				{
+					result = AuthenticationStatus.Failure_AccountNotExist;
+				}
+
+				if ( account != null )
+				{
+					//detach the contact so we don't have to worry about duplicates being committed into the database since this context is going out of scope *very* soon.
+					ctx.Detach( account );
+				}
+			}
+
+			return new AuthenticationResult( result, authenticationAttemptID );
+		}
+
+		public static bool VerifyPasswordMatch( string passwordToTest, string correctPassword, bool passwordIsHashed = false )
+		{
+			bool result = false;
+			if ( passwordIsHashed )
+			{
+				SHA1 sha = SHA1.Create();
+				byte[] correctHashedPasswordBytes = sha.ComputeHash( Encoding.UTF8.GetBytes( correctPassword ) );
+				string correctHashedPasswordString = Convert.ToBase64String( correctHashedPasswordBytes );
+
+				if ( passwordToTest == correctHashedPasswordString )
+				{
+					result = true;
+				}
+			}
+			else
+			{
+				if ( passwordToTest == correctPassword )
+				{
+					result = true;
+				}
+			}
+			return result;
+		}
+
+		public static bool VerifyPasswordMatch( string passwordToTest, byte[] encryptedCorrectPassword, bool passwordIsHashed = false )
+		{
+			string decryptedCorrectPassword = Symmetric.Decrypt( encryptedCorrectPassword );
+			return VerifyPasswordMatch( passwordToTest, decryptedCorrectPassword, passwordIsHashed );
+		}
+
+		private void LoginButton_Click( object sender, RoutedEventArgs e )
+		{
+			Account account;
+
+            OutputTB.Clear();
+			account = CoreDataRepository.Accounts.GetByUserName( useridTB.Text );
+			AuthenticationResult result = Authenticate( useridTB.Text, passwordTB.Password, out account );
+            if (result.Status == AuthenticationStatus.Success)
+            {
+                CurrentAccountID = account.ID;
+                Contact contact = Repository.Contacts.GetByAccount(account);
+                TestControl testControl = new TestControl();
+                testControl.DataRepository = DataRepository;
+                this.Content = testControl;
+            }
+            else
+            {
+                OutputTB.Text = "Invalid Login";
+            }
+		}
+	}
+}
---
+++ cers/Source/CERS.Windows.SubmittalDelta/TestControl.xaml.cs
@@ -0,0 +1,231 @@
+﻿using CERS;
+using CERS.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using UPF;
+using UPF.Core;
+using UPF.Core.Cryptography;
+using UPF.Core.Model;
+
+namespace CERS.Windows.SubmittalDelta
+{
+	/// <summary>
+	/// Interaction logic for TestControl.xaml
+	/// </summary>
+	public partial class TestControl : UserControl
+	{
+		public TestControl()
+		{
+			InitializeComponent();
+			SDFsidTB.Text = "100678";
+		}
+
+		public ICERSRepositoryManager DataRepository { get; set; }
+
+		private void ClearButton_Click( object sender, RoutedEventArgs e )
+		{
+			OutputTB.Clear();
+		}
+
+		private XElement FSERHtml( XElement fser )
+		{
+			XElement outer = new XElement( "div" );
+			IEnumerable<XElement> fields = fser.Descendants( "Field" );
+
+			if ( fields.Count() > 0 )
+			{
+				XElement fieldOuter =
+					new XElement( "div",
+						new XElement( "h3", fser.Attribute( "ResourceType" ).Value ) );
+
+				XElement table =
+					new XElement( "table",
+						new XElement( "tr",
+							new XElement( "th", "Field Name" ),
+							new XElement( "th", "New Value" ),
+							new XElement( "th", "Old Value" ) ) );
+				fieldOuter.Add( table );
+
+				foreach ( XElement field in fields )
+				{
+					table.Add(
+						new XElement( "tr",
+							new XElement( "td", field.Attribute( "Name" ).Value ),
+							new XElement( "td", field.Attribute( "NewValue" ).Value ),
+							new XElement( "td", field.Attribute( "OldValue" ).Value ) ) );
+				}
+				outer.Add( fieldOuter );
+			}
+
+			IEnumerable<XElement> docs = fser.Descendants( "Document" );
+
+			if ( docs.Count() > 0 )
+			{
+				XElement docOuter =
+					new XElement( "div",
+						new XElement( "h3", fser.Attribute( "ResourceType" ).Value ) );
+
+				XElement table =
+					new XElement( "table",
+						new XElement( "tr",
+							new XElement( "th", "Change" ),
+							new XElement( "th", "Option" ),
+							new XElement( "th", "Document Name" ) ) );
+				docOuter.Add( table );
+
+				foreach ( XElement doc in docs )
+				{
+					table.Add(
+						new XElement( "tr",
+							new XElement( "td", doc.Attribute( "Change" ).Value ),
+							new XElement( "td", doc.Attribute( "Option" ).Value ),
+							new XElement( "td", doc.Attribute( "DocumentName" ).Value ) ) );
+				}
+				outer.Add( docOuter );
+			}
+
+			return outer;
+		}
+
+		private XElement GenerateHtml( XElement deltaElement )
+		{
+			XElement fse = deltaElement.Descendants( "FSE" ).FirstOrDefault();
+
+			XElement divMain =
+				new XElement( "div",
+					new XElement( "h2", fse.Attribute( "SubmittalElementType" ).Value ) );
+
+			IEnumerable<XElement> htmlElements =
+				( from fser in fse.Descendants( "FSER" )
+				  select FSERHtml( fser ) );
+
+			divMain.Add( htmlElements );
+			return divMain;
+		}
+
+		private void GenerateJson( XElement deltaElement )
+		{
+			var fieldRows =
+				from fser in deltaElement.Descendants( "FSER" )
+				from field in fser.Descendants( "Field" )
+				select new
+				{
+					fser = fser.Attribute( "ResourceType" ).Value,
+					fieldName = field.Attribute( "Name" ).Value,
+					newValue = field.Attribute( "NewValue" ).Value,
+					oldValue = field.Attribute( "OldValue" ).Value
+				};
+		}
+
+		private void InventorySummaryButton_Click( object sender, RoutedEventArgs e )
+		{
+			ICERSSystemServiceManager services = ServiceLocator.GetSystemServiceManager( DataRepository );
+
+			try
+			{
+				int fsid = 0;
+				fsid = Convert.ToInt32( ISFsidTB.Text );
+
+				services.BusinessLogic.SubmittalElements.HazardousMaterialsInventory.InventorySummary.Summary( Convert.ToInt32( fsid ) );
+
+				// all this was done in Summary but we need the information to get the output
+				FacilitySubmittal fs = ( from fsRec in DataRepository.DataModel.FacilitySubmittals where fsRec.ID == fsid select fsRec ).FirstOrDefault();
+				FacilitySubmittalElement fse =
+					( from fseRec in DataRepository.DataModel.FacilitySubmittalElements
+					  where fseRec.FacilitySubmittal.ID == fs.ID && fseRec.Voided == false
+						  && fseRec.SubmittalElementID == (int) SubmittalElementType.HazardousMaterialsInventory
+					  select fseRec ).FirstOrDefault();
+				FacilitySubmittalElementResource fser =
+					( from fserRec in DataRepository.DataModel.FacilitySubmittalElementResources
+					  where fserRec.FacilitySubmittalElementID == fse.ID
+						  && fserRec.Voided == false && fserRec.ResourceTypeID == (int) ResourceType.HazardousMaterialInventory
+					  select fserRec ).FirstOrDefault();
+
+				InventorySummary inventory = DataRepository.InventorySummaries.GetByFSERID( fser.ID );
+				StringBuilder results = new StringBuilder();
+				results.Append( "Inventory Summary  FSE: " + fse.ID + " CERSID: " + fse.CERSID + "\n" );
+
+				results.Append( "   Solid Material Count: " + inventory.SolidMaterialCount + " EHS Count: " +
+					inventory.SolidEHSCount + " Maximum Daily Value: " + inventory.SolidMaximumDailyValue + " pounds\n" );
+				results.Append( "   Liquid Material Count: " + inventory.LiquidMaterialCount + " EHS Count: " +
+					inventory.LiquidEHSCount + " Maximum Daily Value: " + inventory.LiquidMaximumDailyValue + " gallons\n" );
+				results.Append( "   Gas Material Count: " + inventory.GasMaterialCount + " EHS Count: " +
+					inventory.GasEHSCount + " Maximum Daily Value: " + inventory.GasMaximumDailyValue + " cubic feet\n" );
+				results.Append( "   Unique Location Count: " + inventory.UniqueLocationCount + "\n" );
+
+				this.OutputTB.Text = results.ToString();
+			}
+			catch ( Exception ex )
+			{
+				this.OutputTB.Text = "Error: " + ex.Message;
+			}
+		}
+
+		private void SummaryDeltaButton_Click( object sender, RoutedEventArgs e )
+		{
+			ICERSSystemServiceManager services = ServiceLocator.GetSystemServiceManager( DataRepository );
+
+			try
+			{
+				int fsid = 0;
+				fsid = Convert.ToInt32( SDFsidTB.Text );
+				XElement submittalDeltaElement = services.SubmittalDelta.DeltaFS( fsid );
+				this.OutputTB.Text = submittalDeltaElement.ToString();
+			}
+			catch ( Exception ex )
+			{
+				this.OutputTB.Text = "Error: " + ex.Message;
+			}
+		}
+
+		private void SummaryDeltaFSEButton_Click( object sender, RoutedEventArgs e )
+		{
+			ICERSSystemServiceManager services = ServiceLocator.GetSystemServiceManager( DataRepository );
+
+			try
+			{
+				int fseid = 0;
+				fseid = Convert.ToInt32( SDFseidTB.Text );
+                XElement submittalDeltaElement = services.SubmittalDelta.ProcessDeltaFSE(fseid);
+				//XElement htmlElement = GenerateHtml(submittalDeltaElement);
+				//this.OutputTB.Text = htmlElement.ToString();
+				//GenerateJson(submittalDeltaElement);
+
+				this.OutputTB.Text = submittalDeltaElement.ToString();
+			}
+			catch ( Exception ex )
+			{
+				this.OutputTB.Text = "Error: " + ex.Message;
+			}
+		}
+
+		private void UserControl_SizeChanged( object sender, SizeChangedEventArgs e )
+		{
+			if ( e.WidthChanged )
+			{
+				if ( e.NewSize.Width > 640 )
+				{
+					OutputTB.Width = e.NewSize.Width - 40;
+				}
+				else
+				{
+					OutputTB.Width = 600;
+				}
+			}
+		}
+	}
+}
