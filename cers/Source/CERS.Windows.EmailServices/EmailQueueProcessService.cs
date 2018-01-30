dca97eeefd8d51223466713576acec61ce2c48e6
hdpham77 <35605599+hdpham77@users.noreply.github.com>
2018-01-30 15:07:47 -0800

    Add files via upload

---
+++ cers/Source/CERS.Windows.EmailServices/EmailQueueProcessService.cs
@@ -0,0 +1,52 @@
+﻿using CERS.ServiceProcesses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CERS.Windows.EmailServices
+{
+	public partial class EmailQueueProcessService : ServiceBase
+	{
+		private EmailServiceProcess _Process;
+
+		public EmailQueueProcessService()
+		{
+			InitializeComponent();
+			RegenerateServiceName();
+			_Process = new EmailServiceProcess();
+		}
+
+		protected override void OnStart( string[] args )
+		{
+			_Process.Start();
+		}
+
+		protected override void OnStop()
+		{
+			_Process.Stop();
+		}
+
+		protected virtual void RegenerateServiceName()
+		{
+#if DEVELOPMENT
+			this.ServiceName += "_DEVELOPMENT";
+#elif PRODUCTION
+			this.ServiceName += "_PRODUCTION";
+#elif STAGING
+			this.ServiceName += "_STAGING";
+#elif TRAINING
+			this.ServiceName += "_TRAINING";
+#elif TESTING
+			this.ServiceName += "_TESTING";
+#elif DOCUMENTATION
+			this.ServiceName += "_DOCUMENTATION";
+#endif
+		}
+	}
+}
---
+++ cers/Source/CERS.Windows.EmailServices/ProjectInstaller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+using UPF;
+
+namespace CERS.Windows.EmailServices
+{
+	[RunInstaller( true )]
+	public partial class ProjectInstaller : System.Configuration.Install.Installer
+	{
+		public ProjectInstaller()
+		{
+			InitializeComponent();
+			RegenerateServiceNames();
+		}
+
+		protected virtual void RegenerateServiceNames()
+		{
+#if DEVELOPMENT
+
+			RegenerateServicesNames(
+				RuntimeEnvironment.Development
+			);
+
+#elif PRODUCTION
+			RegenerateServicesNames(
+				RuntimeEnvironment.Production
+			);
+#elif STAGING
+			RegenerateServicesNames(
+				RuntimeEnvironment.Staging
+			);
+#elif TRAINING
+			RegenerateServicesNames(
+				RuntimeEnvironment.Training
+			);
+#elif TESTING
+            RegenerateServicesNames(
+                RuntimeEnvironment.Testing
+            );
+#elif DOCUMENTATION
+			RegenerateServicesNames(
+				RuntimeEnvironment.Documentation
+			);
+#endif
+		}
+
+		protected virtual void RegenerateServicesNames( RuntimeEnvironment runtimeEnvironment )
+		{
+			RegenerateServicesNamesForServices(
+				runtimeEnvironment,
+				siEmailService
+			);
+		}
+
+		protected virtual void RegenerateServicesNamesForServices( RuntimeEnvironment runtimeEnvironment, params ServiceInstaller[] serviceInstallers )
+		{
+			foreach ( ServiceInstaller serviceInstaller in serviceInstallers )
+			{
+				serviceInstaller.ServiceName += "_" + runtimeEnvironment.ToString().ToUpper();
+				serviceInstaller.Description += " (" + runtimeEnvironment.ToString() + " Environment)";
+				serviceInstaller.DisplayName += " (" + runtimeEnvironment.ToString() + ")";
+			}
+		}
+	}
+}
